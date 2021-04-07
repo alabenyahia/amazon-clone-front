077b1d40ffb02b3b183c25aef409b47479993d82
Ala Ben Yahia <alabny18@gmail.com>
2021-04-07 17:59:15 +0100

    Add home page banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,9 @@ function Home(props) {
     return (
         <Container>
             <Navbar />
+            <MainContainer>
+                <Banner />
+            </MainContainer>
         </Container>
     );
 }
@@ -13,3 +16,16 @@ function Home(props) {
 export default Home;
 
 const Container = styled.div``;
+const MainContainer = styled.div`
+    width: 100%;
+    max-width: 1500px;
+    margin: 0 auto;
+`;
+const Banner = styled.div`
+    background: url("https://images-na.ssl-images-amazon.com/images/G/01/AmazonMusic/2021/Marketing/EvergreenQ1/Gateway/US-EN_AMU_EvergreenQ1_DMUX-3799_JZ_OnS_GW_Hero_D_1500x600_1X_CV1._CB412009348_.jpg")
+        no-repeat center center;
+    background-size: cover;
+    width: 100%;
+    height: 600px;
+    mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+`;
